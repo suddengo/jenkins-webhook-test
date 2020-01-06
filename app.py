2e13ff7bc0c07a0821da13dbfbe24f0065610da8
tanwo <1205861166@qq.com>
2020-01-06 10:39:43 +0800

    001 app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloworld_views():
-    return 'Hello, World!'
+    return 'Hello, World! 001'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=9999)
